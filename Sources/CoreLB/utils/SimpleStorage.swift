dd55c945e4df8fd45cdd5b22a4f5e666e898a10b
Alex <ohaliatkin@competo.io>
2023-04-07 00:07:12 +0300

    simplify storage

--- Sources/CoreLB/utils/SimpleStorage.swift
+++ Sources/CoreLB/utils/SimpleStorage.swift
@@ -11,6 +11,12 @@ import Foundation
 open class SimpleStorage{
     
     
+    
+    private let encoder = JSONEncoder()
+    
+    private let decoder = JSONDecoder()
+    
+    
     public init() {}
     
     @discardableResult
@@ -21,16 +27,31 @@ open class SimpleStorage{
     
     public func readInt(_ key: String) -> Int{
         return UserDefaults.standard.integer(forKey: key)
-    }
- 
-    @discardableResult
-    public func updateObj(_ data: Data, _ key: String) -> Bool{
-        UserDefaults.standard.set(data, forKey: key)
-        return true
+    }    
+    
+    public func getObject<T: Codable>(key: String) -> T?{
+        if let data = UserDefaults.standard.data(forKey: key) {
+            do {
+                return try decoder.decode(T.self, from: data)
+            } catch {
+                print("Unable to Decode \(String(describing: T.self)) (\(error))")
+                return nil
+            }
+        }
+        return nil
     }
     
-    public func readObj(_ key: String) -> Data?{
-        return UserDefaults.standard.data(forKey: key)
+    
+    @discardableResult
+    public func saveObject<T: Codable>(_ model: T, key: String) -> Bool{
+        do {
+            let data = try encoder.encode(model)
+            UserDefaults.standard.set(data, forKey: key)
+            return true
+        } catch {
+            print("Unable to Encode \(String(describing: T.self)) (\(error))")
+            return false
+        }
     }
     
 }
